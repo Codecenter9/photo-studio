7dd54f004c58c043c8b6d428475fee6d75e71a39
Juhar E. <juhar8264@gmail.com>
2026-03-06 16:09:51 +0300

    api/token route error fixed

--- app/api/public/gallery/[token]/route.ts
+++ app/api/public/gallery/[token]/route.ts
@@ -1,17 +1,15 @@
-// app/api/public/gallery/[token]/route.ts
-
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import dbConnection from "@/lib/mongodb";
 import ClientShare from "@/model/ClientShare";
 import File from "@/model/File";
-import { use } from "react";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { token: string } }
+  request: NextRequest,
+  context: { params: Promise<{ token: string }> }
 ) {
   await dbConnection();
-  const { token } = await params;
+
+  const { token } = await context.params;
 
   const share = await ClientShare.findOne({
     token: token,
